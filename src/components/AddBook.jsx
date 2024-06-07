547606a69f12d12e200350293b1b15100611a1e8
jinithajohnson <jinithajohnson02@gmail.com>
2024-06-07 22:11:19 +0530

    form

---
+++ src/components/AddBook.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AddBook = () => {
+  return (
+    <div>
+    <NavBar/>
+    <div className="container">
+        <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">book title</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">category</label>
+                    <select name="" id="" className="form-control">
+                        <option value="science">science</option>
+                        <option value="maths">maths</option>
+                        <option value="Autobiography">Autobiography</option>
+                        <option value="Fiction">Fiction</option>
+                        <option value="Commerce">Commerce</option>
+                    </select>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <laber className="form-label">publish year</laber>
+                    <input type="date" name="" id="" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Author name</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label htmlFor="" className="form-label">Description</label>
+                    <textarea name="" id="" className="form-control"></textarea>
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Distributor name</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Publisher name</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">price</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">Edition</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-success">Submit</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+    </div>
+  )
+}
+
+export default AddBook
---
+++ src/components/View.jsx
@@ -0,0 +1,111 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const View = () => {
+  return (
+    <div>
+<NavBar/>
+        <div className="container">
+        <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+<div className="row g-3">
+    <div className="col col-12 col-sm-12 col-md-12 col-xl-12 col-xxl-12">
+
+    <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Book title</th>
+      <th scope="col">Author</th>
+      <th scope="col">Publisher</th>
+      <th scope="col">Price</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+    <tr>
+    <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+    <tr>
+    <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+    <tr>
+    <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+    <tr>
+    <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+    <tr>
+    <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+    <tr>
+    <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+    <tr>
+    <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+    <tr>
+    <td scope="row">1</td>
+      <td>wings</td>
+      <td>Otto</td>
+      <td>yono</td>
+      <td>400</td>
+    </tr>
+
+  </tbody>
+</table>
+
+
+
+
+
+    </div>
+</div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+    </div>
+  )
+}
+
+export default View
